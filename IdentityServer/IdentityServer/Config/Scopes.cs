f03683018323112a96d03ceae04e430781f76c46
ajay.kotnala <ajay.kotnala@aveva.com>
2016-04-12 14:21:00 +0530

    added IDSRV3 in working state

---
+++ IdentityServer/IdentityServer/Config/Scopes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IdentityServer3.Core.Models;
+
+namespace IdentityServer.Config
+{
+    public static class Scopes
+    {
+        public static IEnumerable<Scope> Get()
+        {
+            return new List<Scope>
+            {
+                new Scope
+                {
+                    Name = "galarymanagement",
+                    DisplayName = "Galary Management",
+                    Description = "Token has to contiain the scope to enable access to api's",
+                    Type = ScopeType.Resource
+                }
+            };
+        } 
+    }
+}
