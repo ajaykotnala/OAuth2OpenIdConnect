535fe14330701fdb7565a024e04e2bbbd73c7a65
ajay.kotnala <ajay.kotnala@aveva.com>
2016-04-12 17:42:44 +0530

    added WEBAPI/Anguarjs project

---
+++ AngularjsWeb/App/app.js
@@ -0,0 +1,23 @@
+﻿(function(angular) {
+    "use strict";
+
+    var app = angular.module('app', [
+       'ui.router'
+    ]);
+
+    /* jshint ignore:start */
+    //app.run(function ($state) { }); /* Required to force the first state to be displayed. */
+    ///* jshint ignore:end */
+
+    app.config(function($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise("/welcome");
+
+        $stateProvider
+            .state('welcome', {
+                url: '/welcome',
+                templateUrl: 'App/templates/welcome.html',
+                controller: 'WelcomeController'
+            });
+    });
+})(angular);
---
+++ AngularjsWeb/App/scripts/WelcomeController.js
@@ -0,0 +1,9 @@
+﻿(function (angular) {
+    'use strict';
+
+    angular.module('app').controller('WelcomeController', WelcomeController);
+
+    function WelcomeController($scope) {
+        $scope.message = "This message is coming from Controller";
+    }
+})(angular);
